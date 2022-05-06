a09d427d7064a88549b908b95b2ebb8ee90f431a
Anagha Khanolkar <akhanolkar@google.com>
2022-05-05 23:30:00 -0500

    Initial commit

---
+++ lab-01/cell-tower-anomaly-detection/00-scripts/cell_tower_performance_indicator.py
@@ -0,0 +1,96 @@
+import configparser
+from datetime import datetime
+import os
+import json
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col, substring, lit, when, avg
+from pyspark.sql import functions as F
+from pyspark.sql.functions import input_file_name
+import random
+from pyspark.sql.types import *
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, dayofweek, date_format
+from pyspark import SparkContext, SparkConf, SQLContext
+from google.cloud import storage
+from google.cloud import bigquery
+import sys
+
+#Reading the arguments and storing them in variables
+project_name=sys.argv[1]
+data_set=sys.argv[2]
+BUCKET_NAME=sys.argv[3]
+user_name=sys.argv[4]
+
+input_data="gs://"+BUCKET_NAME+"/cell-tower-anomaly-detection/output_data/"+user_name+"_customer_threshold_service_join/part*"
+output_data="gs://"+BUCKET_NAME+"/cell-tower-anomaly-detection/output_data"
+
+# Building the Spark Session
+spark =SparkSession.builder.appName("cell_tower_performance_dataset-exploration").config('spark.jars', 'gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar').getOrCreate()
+
+#Reading the Input Data
+custDF1 = spark.read.format("parquet").option("header", True).option("inferschema",True).load(input_data)
+custDF1.printSchema()
+
+# Layer2 slicing done at avg of threshold's for cellname granularity level on top of Layer1 level
+custDF1=custDF1.drop(custDF1.months)
+custDF1.createOrReplaceTempView("slice1")
+custDF2 = spark.sql('''select *,  ROW_NUMBER()  OVER(PARTITION BY customerID ORDER BY customerID) AS month from slice1   ''')
+custDF2.createOrReplaceTempView("slice1_1")
+custDF3 = spark.sql('''select customerID,CellName,tenure,PhoneService,MultipleLines,InternetService,avg(PRBUsageUL) as avg_PRBUsageUL,avg(PRBUsageDL) as avg_PRBUsageDL,avg(meanThr_DL) as avg_meanThr_DL,avg(meanThr_UL) as avg_meanThr_UL,avg(maxThr_DL) as avg_maxThr_DL,avg(maxThr_UL) as 	avg_maxThr_UL,avg(meanUE_DL) as avg_meanUE_DL,avg(meanUE_UL) as avg_meanUE_UL,avg(maxUE_DL) as avg_maxUE_DL,avg(maxUE_UL) as avg_maxUE_UL,avg(maxUE_UL_DL) as avg_maxUE_UL_DL,avg(Unusual) as avg_Unusual,avg(roam_Mean) as avg_roam_Mean,avg(change_mou) as avg_change_mou,avg(drop_vce_Mean) as avg_drop_vce_Mean,avg(drop_dat_Mean) as avg_drop_dat_Mean,avg(blck_vce_Mean) as avg_blck_vce_Mean,avg(blck_dat_Mean) as avg_blck_dat_Mean,avg(plcd_vce_Mean) as avg_plcd_vce_Mean,avg(plcd_dat_Mean) as avg_plcd_dat_Mean,avg(comp_vce_Mean) as avg_comp_vce_Mean,avg(comp_dat_Mean) as avg_comp_dat_Mean,avg(peak_vce_Mean) as avg_peak_vce_Mean,avg(peak_dat_Mean) as avg_peak_dat_Mean,avg(mou_peav_Mean) as avg_mou_peav_Mean,avg(mou_pead_Mean) as avg_mou_pead_Mean,avg(opk_vce_Mean) as avg_opk_vce_Mean,avg(opk_dat_Mean) as avg_opk_dat_Mean,avg(mou_opkv_Mean) as avg_mou_opkv_Mean,avg(mou_opkd_Mean) as avg_mou_opkd_Mean,avg(drop_blk_Mean) as avg_drop_blk_Mean,avg(callfwdv_Mean) as avg_callfwdv_Mean,avg(callwait_Mean) as avg_callwait_Mean  from slice1_1 where PhoneService = 'Yes'  group by customerID,CellName,tenure,PhoneService,MultipleLines,InternetService  ''')
+
+custDF3.createOrReplaceTempView("slice2")
+custDF4 = spark.sql('''select CellName,count(customerID) as customerID_count,avg(avg_PRBUsageUL) as avg_PRBUsageUL,avg(avg_PRBUsageDL) as avg_PRBUsageDL,avg(avg_meanThr_DL) as avg_meanThr_DL,avg(avg_meanThr_UL) as avg_meanThr_UL,avg(avg_maxThr_DL) as avg_maxThr_DL,avg(avg_maxThr_UL) as avg_maxThr_UL,avg(avg_meanUE_DL) as avg_meanUE_DL,avg(avg_meanUE_UL) as avg_meanUE_UL,avg(avg_maxUE_DL) as avg_maxUE_DL,avg(avg_maxUE_UL) as avg_maxUE_UL,avg(avg_maxUE_UL_DL) as avg_maxUE_UL_DL,avg(avg_Unusual) as avg_Unusual,avg(avg_roam_Mean) as avg_roam_Mean,avg(avg_change_mou) as avg_change_mou,avg(avg_drop_vce_Mean) as avg_drop_vce_Mean,avg(avg_drop_dat_Mean) as avg_drop_dat_Mean,avg(avg_blck_vce_Mean) as avg_blck_vce_Mean,avg(avg_blck_dat_Mean) as avg_blck_dat_Mean,avg(avg_plcd_vce_Mean) as avg_plcd_vce_Mean,avg(avg_plcd_dat_Mean) as avg_plcd_dat_Mean,avg(avg_comp_vce_Mean) as avg_comp_vce_Mean,avg(avg_comp_dat_Mean) as avg_comp_dat_Mean,avg(avg_peak_vce_Mean) as avg_peak_vce_Mean,avg(avg_peak_dat_Mean) as avg_peak_dat_Mean,avg(avg_mou_peav_Mean) as avg_mou_peav_Mean,avg(avg_mou_pead_Mean) as avg_mou_pead_Mean,avg(avg_opk_vce_Mean) as avg_opk_vce_Mean,avg(avg_opk_dat_Mean) as avg_opk_dat_Mean,avg(avg_mou_opkv_Mean) as avg_mou_opkv_Mean,avg(avg_mou_opkd_Mean) as avg_mou_opkd_Mean,avg(avg_drop_blk_Mean) as avg_drop_blk_Mean,avg(avg_callfwdv_Mean) as avg_callfwdv_Mean,avg(avg_callwait_Mean) as avg_callwait_Mean  from slice2 group by CellName''')
+
+# Performace check on top of Layer2 for cell tower level
+custDF5=custDF4.withColumn('service_stability_voice_calls',custDF4.avg_peak_vce_Mean/custDF4.avg_opk_vce_Mean   )
+custDF6=custDF5.withColumn('service_stability_data_calls',custDF4.avg_peak_dat_Mean/custDF4.avg_opk_dat_Mean )
+custDF7=custDF6.withColumn('Incomplete_voice_calls',custDF4.avg_plcd_vce_Mean -custDF4.avg_comp_vce_Mean )
+custDF8=custDF7.withColumn('Incomplete_data_calls',custDF4.avg_plcd_dat_Mean -custDF4.avg_comp_dat_Mean )
+
+custDF9=custDF8.withColumn('PRBUsageUL_Thrsld',when(col("avg_PRBUsageUL") < str(custDF8.select(avg("avg_PRBUsageUL")).collect()[0][0]), 0)             .when(col("avg_PRBUsageUL") >str(custDF8.select(avg("avg_PRBUsageUL")).collect()[0][0]),1) )
+custDF10=custDF9.withColumn('PRBUsageDL_Thrsld', when(col("avg_PRBUsageDL") < str(custDF8.select(avg("avg_PRBUsageDL")).collect()[0][0]), 0)              .when(col("avg_PRBUsageDL") > str(custDF8.select(avg("avg_PRBUsageDL")).collect()[0][0]), 1))
+custDF11=custDF10.withColumn('meanThr_DL_Thrsld', when(col("avg_meanThr_DL") < str(custDF8.select(avg("avg_meanThr_DL")).collect()[0][0]), 1)              .when(col("avg_meanThr_DL") > str(custDF8.select(avg("avg_meanThr_DL")).collect()[0][0]), 0) )
+custDF12=custDF11.withColumn('meanThr_UL_Thrsld', when(col("avg_meanThr_UL") < str(custDF8.select(avg("avg_meanThr_UL")).collect()[0][0]), 1)              .when(col("avg_meanThr_UL") > str(custDF8.select(avg("avg_meanThr_UL")).collect()[0][0]), 0) )
+custDF13=custDF12.withColumn('maxThr_DL_Thrsld', when(col("avg_maxThr_DL") < str(custDF8.select(avg("avg_maxThr_DL")).collect()[0][0]), 0)              .when(col("avg_maxThr_DL") > str(custDF8.select(avg("avg_maxThr_DL")).collect()[0][0]), 1))
+custDF14=custDF13.withColumn('maxThr_UL_Thrsld', when(col("avg_maxThr_UL") <str(custDF8.select(avg("avg_maxThr_UL")).collect()[0][0]), 0)              .when(col("avg_maxThr_UL") > str(custDF8.select(avg("avg_maxThr_UL")).collect()[0][0]), 1))
+custDF15=custDF14.withColumn('meanUE_DL_Thrsld', when(col("avg_meanUE_DL") < str(custDF8.select(avg("avg_meanUE_DL")).collect()[0][0]), 0)             .when(col("avg_meanUE_DL") > str(custDF8.select(avg("avg_meanUE_DL")).collect()[0][0]), 1))
+custDF16=custDF15.withColumn('meanUE_UL_Thrsld', when(col("avg_meanUE_UL") < str(custDF8.select(avg("avg_meanUE_UL")).collect()[0][0]), 0)              .when(col("avg_meanUE_UL") >str(custDF8.select(avg("avg_meanUE_UL")).collect()[0][0]), 1) )
+custDF17=custDF16.withColumn('maxUE_DL_Thrsld',when(col("avg_maxUE_DL") < str(custDF8.select(avg("avg_maxUE_DL")).collect()[0][0]), 0)              .when(col("avg_maxUE_DL") > str(custDF8.select(avg("avg_maxUE_DL")).collect()[0][0]), 1))
+custDF18=custDF17.withColumn('maxUE_UL_Thrsld', when(col("avg_maxUE_UL") < str(custDF8.select(avg("avg_maxUE_UL")).collect()[0][0]), 0)              .when(col("avg_maxUE_UL") > str(custDF8.select(avg("avg_maxUE_UL")).collect()[0][0]), 1) )
+custDF19=custDF18.withColumn('maxUE_UL_DL_Thrsld', when(col("avg_maxUE_UL_DL") < str(custDF8.select(avg("avg_maxUE_UL_DL")).collect()[0][0]), 0)              .when(col("avg_maxUE_UL_DL") > str(custDF8.select(avg("avg_maxUE_UL_DL")).collect()[0][0]), 1) )
+custDF20=custDF19.withColumn('roam_Mean_Thrsld', when(col("avg_roam_Mean") < str(custDF8.select(avg("avg_roam_Mean")).collect()[0][0]), 1)              .when(col("avg_roam_Mean") > str(custDF8.select(avg("avg_roam_Mean")).collect()[0][0]), 0) )
+custDF21=custDF20.withColumn('change_mouL_Thrsld', when(col("avg_change_mou") < str(custDF8.select(avg("avg_change_mou")).collect()[0][0]), 1)              .when(col("avg_change_mou") > str(custDF8.select(avg("avg_change_mou")).collect()[0][0]), 0) )
+custDF22=custDF21.withColumn('drop_vce_Mean_Thrsld', when(col("avg_drop_vce_Mean") < str(custDF8.select(avg("avg_drop_vce_Mean")).collect()[0][0]), 0)              .when(col("avg_drop_vce_Mean") > str(custDF8.select(avg("avg_drop_vce_Mean")).collect()[0][0]), 1))
+custDF23=custDF22.withColumn('drop_dat_Mean_Thrsld', when(col("avg_drop_dat_Mean") < str(custDF8.select(avg("avg_drop_dat_Mean")).collect()[0][0]), 0)              .when(col("avg_drop_dat_Mean") > str(custDF8.select(avg("avg_drop_dat_Mean")).collect()[0][0]), 1) )
+custDF24=custDF23.withColumn('blck_vce_Mean_Thrsld', when(col("avg_blck_vce_Mean") < str(custDF8.select(avg("avg_blck_vce_Mean")).collect()[0][0]), 0)              .when(col("avg_blck_vce_Mean") > str(custDF8.select(avg("avg_blck_vce_Mean")).collect()[0][0]), 1) )
+custDF25=custDF24.withColumn('blck_dat_Mean_Thrsld', when(col("avg_blck_dat_Mean") < str(custDF8.select(avg("avg_blck_dat_Mean")).collect()[0][0]), 0)              .when(col("avg_blck_dat_Mean") > str(custDF8.select(avg("avg_blck_dat_Mean")).collect()[0][0]), 1) )
+custDF26=custDF25.withColumn('peak_vce_Mean_Thrsld', when(col("avg_peak_vce_Mean") < str(custDF8.select(avg("avg_peak_vce_Mean")).collect()[0][0]), 1)              .when(col("avg_peak_vce_Mean") > str(custDF8.select(avg("avg_peak_vce_Mean")).collect()[0][0]), 0) )
+custDF27=custDF26.withColumn('peak_dat_Mean_Thrsld', when(col("avg_peak_dat_Mean") < str(custDF8.select(avg("avg_peak_dat_Mean")).collect()[0][0]), 1)              .when(col("avg_peak_dat_Mean") > str(custDF8.select(avg("avg_peak_dat_Mean")).collect()[0][0]), 0))
+custDF28=custDF27.withColumn('opk_vce_Mean_Thrsld', when(col("avg_opk_vce_Mean") < str(custDF8.select(avg("avg_opk_vce_Mean")).collect()[0][0]), 1)              .when(col("avg_opk_vce_Mean") > str(custDF8.select(avg("avg_opk_vce_Mean")).collect()[0][0]), 0) )
+custDF29=custDF28.withColumn('opk_dat_Mean_Thrsld', when(col("avg_opk_dat_Mean") < str(custDF8.select(avg("avg_opk_dat_Mean")).collect()[0][0]), 1)              .when(col("avg_opk_dat_Mean") > str(custDF8.select(avg("avg_opk_dat_Mean")).collect()[0][0]), 0))
+custDF30=custDF29.withColumn('drop_blk_Mean_Thrsld',  when(col("avg_drop_blk_Mean") < str(custDF8.select(avg("avg_drop_blk_Mean")).collect()[0][0]), 1)              .when(col("avg_drop_blk_Mean") > str(custDF8.select(avg("avg_drop_blk_Mean")).collect()[0][0]), 0) )
+custDF31=custDF30.withColumn('callfwdv_Mean_Thrsld', when(col("avg_callfwdv_Mean") < str(custDF8.select(avg("avg_callfwdv_Mean")).collect()[0][0]), 1)              .when(col("avg_callfwdv_Mean") > str(custDF8.select(avg("avg_callfwdv_Mean")).collect()[0][0]), 0))
+custDF32=custDF31.withColumn('service_stability_voice_calls_Thrsld', when(col("service_stability_voice_calls")> str(custDF8.select(avg("service_stability_voice_calls")).collect()[0][0]) , 0)              .when(col("service_stability_voice_calls")< str(custDF8.select(avg("service_stability_voice_calls")).collect()[0][0]), 1))
+custDF33=custDF32.withColumn('service_stability_data_calls_Thrsld', when(col("service_stability_data_calls")> str(custDF8.select(avg("service_stability_data_calls")).collect()[0][0]), 0)              .when(col("service_stability_data_calls")< str(custDF8.select(avg("service_stability_data_calls")).collect()[0][0]), 1))
+custDF33.show(truncate=False)
+
+# Based on the performance check verifying whether the Maintainence for the cell tower required or not
+custDF34=custDF33.fillna(value =0)
+custDF35 = custDF34.withColumn("defected_count",col("PRBUsageUL_Thrsld")+col("PRBUsageDL_Thrsld")+col("meanThr_DL_Thrsld")+col("meanThr_UL_Thrsld")+col("maxThr_DL_Thrsld")+col("maxThr_UL_Thrsld")+col("meanUE_DL_Thrsld")+col("meanUE_UL_Thrsld")+col("maxUE_DL_Thrsld")+col("maxUE_UL_Thrsld")+col("maxUE_UL_DL_Thrsld")+col("roam_Mean_Thrsld")+col("change_mouL_Thrsld")+col("drop_vce_Mean_Thrsld")+col("drop_dat_Mean_Thrsld")+col("blck_vce_Mean_Thrsld")+col("blck_dat_Mean_Thrsld")+col("peak_vce_Mean_Thrsld")+col("peak_dat_Mean_Thrsld")+col("opk_vce_Mean_Thrsld")+col("opk_dat_Mean_Thrsld")+col("drop_blk_Mean_Thrsld")+col("callfwdv_Mean_Thrsld")+col("service_stability_voice_calls_Thrsld")+col("service_stability_data_calls_Thrsld"))
+custDF36= custDF35.withColumn("Maintainence_Required",when(col("defected_count")>=15,"Required").otherwise("Not Required"))
+custDF37=custDF36.select(col("CellName"),col("defected_count"),col("Maintainence_Required"))
+custDF37.show(truncate= False)
+
+#Creating bucket's in cloud with the level's of slicing as different datasets
+custDF36.write.parquet(os.path.join(output_data, user_name+"_cell_tower_threshold"), mode = "overwrite")
+
+#Writing the Output data to BigQuery
+bq_client = bigquery.Client(project=project_name)
+
+query = f"""
+CREATE OR REPLACE EXTERNAL TABLE """+data_set+"""."""+user_name+"""_cell_tower_performance_data OPTIONS (
+format = 'PARQUET', uris = ['"""+output_data+"""/"""+user_name+"""_cell_tower_threshold/*.parquet'] );
+"""
+
+job = bq_client.query(query)
+job.result()
+spark.stop()
---
+++ lab-01/cell-tower-anomaly-detection/instructions/06c-cell-tower-anomaly-airflow-execution.md
@@ -0,0 +1,185 @@
+# Cell Tower Anomaly Detection using Serverless Spark through Google Cloud Composer
+
+**Goal** - Detecting anomalous cell towers using network and customer data through serverless.
+
+Following are the lab modules:
+
+[1. Understanding Data](06c-cell-tower-anomaly-airflow-execution.md#1-understanding-data)<br>
+[2. Solution Diagram](06c-cell-tower-anomaly-airflow-execution.md#2-solution-diagram)<br>
+[3. Uploading DAG files to DAGs folder](06c-cell-tower-anomaly-airflow-execution.md#3-uploading-dag-files-to-dags-folder)<br>
+[4. Execution of Airflow DAG](06c-cell-tower-anomaly-airflow-execution.md#4-execution-of-airflow-dag)<br>
+[5. BQ Output Tables](06c-cell-tower-anomaly-airflow-execution.md#5-bq-output-tables)<br>
+[6. Logging](06c-cell-tower-anomaly-airflow-execution.md#6-logging)<br>
+
+## 1. Understanding the data
+
+The datasets used for this project are
+
+1.[telecom_customer_churn_data.csv](01-datasets/telecom_customer_churn_data.csv) <br>
+2.[service_threshold_data.csv](01-datasets/service_threshold_data.csv) <br>
+3.[customer_data](01-datasets/customer_data) <br>
+
+- Telecom Customer Churn Data   - This dataset contains information of services provided to the customers by the celltowers.
+- Service Threshold Data -  This dataset contains the performance metrics thresold information of the celltowers.
+- Cust Raw Data - This is a folder which contains the files which are in parquet format and holds the information of the customer data.
+
+## 2. Solution Diagram
+
+<kbd>
+<img src=images/Flow_of_Resources.jpeg />
+</kbd>
+
+<br>
+<br>
+
+**Model Pipeline**
+
+The model pipeline involves the following steps: <br>
+	- Create buckets in GCS <br>
+	- Create Dataproc and Persistent History Server Cluster <br>
+	- Copy the raw data files, PySpark and notebook files into GCS <br>
+	- Create a Cloud Composer environment and Airflow jobs to run the serverless spark job <br>
+	- Creating Google BigQuery tables with summary of anomalous cell towers <br>
+
+<br>
+
+## 3. Uploading DAG files to DAGs folder
+
+* From the code repository, download the file located at: **cell-tower-anomaly-detection**>**00-scripts**>**cell-tower-airflow.py**
+* Rename file to <your_name_here>-cell-tower-airflow.py
+* Open the file and replace your name on row 21
+* Navigate to **Composer**>**<composer_environment>**
+* Next, navigate to **Environment Configuration**>**DAGs folder URI**
+* Next, upload the DAG file to the GCS bucket corresponding to the **DAGs folder URI**
+
+<kbd>
+<img src=/images/composer_2.png />
+</kbd>
+
+<br>
+<br>
+<br>
+
+<kbd>
+<img src=/images/composer_3.png />
+</kbd>
+
+<br>
+<br>
+<br>
+
+
+## 4. Execution of Airflow DAG
+
+* Navigate to **Composer**>**<your_environment>**>**Open Airflow UI**
+
+<kbd>
+<img src=/images/composer_5.png />
+</kbd>
+
+<br>
+
+* Once the Airflow UI opens, navigate to **DAGs** and open your respective DAG
+* Next, trigger your DAG by clicking on the **Trigger DAG** button
+
+<kbd>
+<img src=/images/composer_6.png />
+</kbd>
+
+<br>
+
+* Once the DAG is triggered, the DAG can be monitored directly through the Airflow UI as well as the Dataproc>Serverless>Batches window
+
+<kbd>
+<img src=/images/composer_7.png />
+</kbd>
+
+<br>
+
+
+## 5. BQ output tables
+
+Navigate to BigQuery Console, and check the **cell_tower_anomaly_detection** dataset. <br>
+Once the Airflow DAG execution is completed, two new tables '<your_name_here>_cell_tower_performance_data' and '<your_name_here>_customer_service_metrics_data' will be created:
+
+<br>
+
+<kbd>
+<img src=images/bq_1.png />
+</kbd>
+
+<br>
+
+To view the data in these tables -
+
+* Select the table from BigQuery Explorer by navigating 'project_id' **>** 'dataset' **>** 'table_name'
+* Click on the **Preview** button to see the data in the table
+
+<br>
+
+<kbd>
+<img src=images/bq_preview.png />
+</kbd>
+
+<br>
+
+**Note:** If the **Preview** button is not visible, run the below queries to view the data. However, these queries will be charged for the full table scan.
+
+```
+  SELECT * FROM `<project_name>.<dataset_name>.<your_name_here>_cell_tower_performance_data` LIMIT 1000;
+  SELECT * FROM `<project_name>.<dataset_name>.<your_name_here>_customer_service_metrics_data` LIMIT 1000;
+
+```
+**Note:** Edit all occurrences of <project_name> and <dataset_name> to match the values of the variables PROJECT_ID, and BQ_DATASET_NAME respectively
+
+<br>
+
+<kbd>
+<img src=images/bq_2.png />
+</kbd>
+
+<br>
+
+## 6. Logging
+
+### 6.1 Airflow logging
+
+* To view the logs of any step of the DAG execution, click on the **<DAG step>**>**Log** button <br>
+
+<kbd>
+<img src=/images/composer_8.png />
+</kbd>
+
+<br>
+
+### 6.2 Serverless Batch logs
+
+Logs associated with the application can be found in the logging console under
+**Dataproc > Serverless > Batches > <batch_name>**.
+<br> You can also click on “View Logs” button on the Dataproc batches monitoring page to get to the logging page for the specific Spark job.
+
+<kbd>
+<img src=/images/image10.png />
+</kbd>
+
+<kbd>
+<img src=/images/image11.png />
+</kbd>
+
+<br>
+
+### 6.3 Persistent History Server logs
+
+To view the Persistent History server logs, click the 'View History Server' button on the Dataproc batches monitoring page and the logs will be shown as below:
+
+<br>
+
+<kbd>
+<img src=/images/image12.png />
+</kbd>
+
+<kbd>
+<img src=/images/image13.png />
+</kbd>
+
+<br>
