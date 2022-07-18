0978c146a4f7d2d80056d93c9b1004fc02343dc7
kmting <kathleen.ting@gmail.com>
2022-07-18 13:00:05 -0700

    Add files via upload

---
+++ lab-04/retail_store_analytics_metastore/00-scripts/retail_analytics_sales_per_dow_per_departmentproduct.py
@@ -0,0 +1,45 @@
+import os
+from pyspark.sql.functions import col
+from pyspark.sql import SparkSession
+from google.cloud import bigquery
+from pyspark.sql.window import Window
+from pyspark.sql.functions import col,avg,sum,min,max,row_number
+from pyspark.sql.functions import round
+import sys
+
+#Reading the arguments and storing them in variables
+project_name=sys.argv[1]
+dataset_name=sys.argv[2]
+bucket_name=sys.argv[3]
+user_name=sys.argv[4]
+database_name=sys.argv[5]
+
+
+
+# Building the Spark Session
+spark = SparkSession.builder.appName('pyspark-retail-inventory').config('spark.jars', 'gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.22.2.jar').getOrCreate()
+
+spark.sql(""" USE {}""".format(database_name));
+
+or_df=spark.sql(""" select * from order_products op inner join orders o on op.order_id =o.order_id """)
+
+df_join=spark.sql(""" select * from products p inner join aisles a on p.aisle_id=a.aisle_id
+inner join departments d on p.department_id=d.department_id """)
+
+df_join = df_join.withColumnRenamed("product_id","product_id_del")
+df=df_join.join(or_df,df_join.product_id_del ==  or_df.product_id,"inner")
+
+windowSpecAgg  = Window.partitionBy("order_dow","department","product_id")
+
+#calculating sales per order_dow_per_department_product
+
+df_1=df.withColumn("sales_per_dow_per_departmentproduct", sum(col("add_to_cart_order")).over(windowSpecAgg)).select("sales_per_dow_per_departmentproduct","department","product_id","aisle","p.aisle_id","order_dow").distinct()
+
+spark.conf.set("parentProject", project_name)
+bucket = bucket_name
+spark.conf.set("temporaryGcsBucket",bucket)
+
+# writing data to bigquery
+df_1.write.format('bigquery') .mode("overwrite").option('table', project_name+':'+dataset_name+'.'+user_name+'_sales_per_dow_per_departmentproduct') .save()
+
+print('Job Completed Successfully!')
