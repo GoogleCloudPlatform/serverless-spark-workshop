8982bbfaacb1b3cd761bd88ebbf96df938265d15
velascoluis <luis.velasco@gmail.com>
2022-08-15 13:31:48 +0200

    Workaround for https://github.com/hashicorp/terraform-provider-google/issues/6782

--- cell-tower-anomaly-detection-dbt/cell-tower-anomaly-detection-dbt/02-config/setup_infra.sh
+++ cell-tower-anomaly-detection-dbt/cell-tower-anomaly-detection-dbt/02-config/setup_infra.sh
@@ -231,6 +231,17 @@ do
   fi
 done
 
+
+# Workaround https://github.com/hashicorp/terraform-provider-google/issues/6782
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1 net.ipv6.conf.default.disable_ipv6=1 net.ipv6.conf.lo.disable_ipv6=1 > /dev/null
+export BUG_APIS="googleapis.com www.googleapis.com storage.googleapis.com iam.googleapis.com container.googleapis.com cloudresourcemanager.googleapis.com"
+for NAME in ${BUG_APIS}
+do
+  ipv4=$(getent ahostsv4 "${NAME}" | head -n 1 | awk '{ print $1 }')
+  grep -q "$name" /etc/hosts || ([ -n "$ipv4" ] && sudo sh -c "echo '$ipv4 $name' >> /etc/hosts")
+done
+# Workaround end
+
 LOG_DATE=`date`
 echo "#################################################################"
 echo "${LOG_DATE} Creating Service Accounts ..."
