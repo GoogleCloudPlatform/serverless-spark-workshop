8e174438388274121b5256a6fd219f327ff64b66
Kate Ting <kathleen.ting@gmail.com>
2022-09-01 11:00:34 -0700

    added license header

--- cell-tower-anomaly-detection-dbt/cell-tower-anomaly-detection-dbt/00-scripts/dbt_bq_spark/models/telco_performance_augmented.sql
+++ cell-tower-anomaly-detection-dbt/cell-tower-anomaly-detection-dbt/00-scripts/dbt_bq_spark/models/telco_performance_augmented.sql
@@ -1,3 +1,6 @@
+-- Copyright 2022 Google LLC.
+-- SPDX-License-Identifier: Apache-2.0
+
 {{ config(materialized='view') }}
 WITH telecomCustomerChurnFinal AS (
     SELECT roam_Mean,change_mou,drop_vce_Mean,drop_dat_Mean,blck_vce_Mean,blck_dat_Mean,plcd_vce_Mean,plcd_dat_Mean,comp_vce_Mean,comp_dat_Mean,peak_vce_Mean,peak_dat_Mean,mou_peav_Mean,mou_pead_Mean,opk_vce_Mean,opk_dat_Mean,mou_opkv_Mean,mou_opkd_Mean,drop_blk_Mean,callfwdv_Mean,callwait_Mean,churn,months,uniqsubs,actvsubs,area,dualband,forgntvl, CAST(SUBSTR(CAST(Customer_ID AS STRING), 4,7) AS INT64) as Customer_ID
